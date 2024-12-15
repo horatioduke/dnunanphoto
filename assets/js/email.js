4a39df46774f7ca2693340acefecc9d5c0468746
Horatio <d.nunan@me.com>
2024-12-15 16:56:52 +0000

    Add files via upload

---
+++ assets/js/email.js
@@ -0,0 +1,20 @@
+function sendMail() {
+    var params = {
+        name: document.getElementById("name").value ,
+        email: document.getElementById("email").value ,
+        message: document.getElementById("message").value ,
+    }
+    const serviceID = "service_ntxvl2v";
+    const templateID = "template_s5dpc7p";
+
+    email.js.send(serviceID,templateID,params)
+    .then(
+        res =>{
+            document.getElementById("name").value = "";
+            document.getElementById("email").value = "";
+            document.getElementById("message").value = "";
+            console.log(res);
+            alert("Your message sent successfully");
+        })
+    .catch(err=>console.log(err));
+}
